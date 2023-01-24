33f0e2dae792c84006b45a940db23a8338cff193
Cody Jennings <cojack91@gmail.com>
2023-01-24 15:00:26 -0500

    updatedendpoint

--- src/services/todos-api.js
+++ src/services/todos-api.js
@@ -1,7 +1,7 @@
 // All of our endpoints
 import axios from 'axios'
 
-const baseURL= 'http://localhost:3001/todos'
+const baseURL= 'https://express-todo-9wo7.onrender.com/todos'
 
 //Show all
 export const getTodos = () => {
